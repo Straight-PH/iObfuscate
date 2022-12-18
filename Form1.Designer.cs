0bd2df415a880278de9e13874c2bfa8864635cdf
Straight <113539768+Straight-PH@users.noreply.github.com>
2022-12-18 07:46:47 -0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace iObfuscate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(-3, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Obfuscate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(-3, -1);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(277, 199);
+            this.textBox1.TabIndex = 2;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(80, 220);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(127, 20);
+            this.maskedTextBox1.TabIndex = 3;
+            this.maskedTextBox1.Text = "File Name With Extension";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(272, 282);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "iObfuscate || Beta";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iObfuscate
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Data = textBox1.Text;
+            string Name = maskedTextBox1.Text;
+
+            StartObfuscation(Data, Name);
+        }
+
+      
+        static void StartObfuscation(string Content, string Name)
+        {
+            string j = "码表示成不同的二进制格式的编码规则";
+            char[] Salt = j.ToCharArray();
+            Random r = new Random();
+            string Junk = "ⓛஐ→あⓛⓞⓥⓔ｡°ºⓛஐ→あⓛⓞⓥⓔ｡°ºஐ→あⓛⓞⓥⓔ｡°º｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°｡°";
+            string UTF16arrow = "→好→处→在→于→大→部→分→字→符→都→以→固→定→长→度→的→字→节→";
+            string UTF16 = "然后从 \"中\" 字的最后一个二进制位开始，按照从后向前的顺序依次填入格式中的 x 字符，多出的二进制补为 0， 具体如上图 步骤2、步骤3 所示然后从 \"中\" 字的最后一个二进制位开始，按照从后向前的顺序依次填入格式中的 x 字符，多出的二进制补为 0， 具体如上图 步骤2、步骤3 所示然后从 \"中\" 字的最后一个二进制位开始，按照从后向前的顺序依次填入格式中的 x 字符，多出的二进制补为 0， 具体如上图 步骤2、步骤3 所示然后从 \"中\" 字的最后一个二进制位开始，按照从后向前的顺序依次填入格式中的 x 字符，多出的二进制补为 0， 具体如上图 步骤2、步骤3 所示";
+            string Alphabets = "abcdefghijklmnopqrstuvwxyz";
+            char[] AlphArray = Alphabets.ToCharArray();
+
+            foreach (char letter in Content)
+            {
+                if (letter == ':')
+                {
+                    Content.Replace(letter, '\n');
+                }
+            }
+
+            File.WriteAllText($"Obfuscated_{Name}", ":: Obfuscated By iobfuscate\n@echo off\n:: %?%@%?%e%?%c%?%h%?%o o%?%f%?%f");
+
+            for (int x = 0; x <= 100; x++)
+            {
+                var buildere = new StringBuilder();
+                int counted = 0;
+                foreach (var c in UTF16)
+                {
+                    buildere.Append(c);
+                    if ((++counted % 1) == 0)
+                    {
+                        buildere.Append("%?%");
+                    }
+                }
+
+                File.AppendAllText($"Obfuscated_{Name}", $":: " + buildere.ToString());
+            }
+            for (int x = 0; x <= 100; x++)
+            {
+                string ContentOB = File.ReadAllText($"Obfuscated_{Name}");
+                string RandValue = RandomStringer();
+                string RandomVar = RandomVariabler();
+
+                if (!ContentOB.Contains(RandValue))
+                {
+                    File.AppendAllText($"Obfuscated_{Name}", $"\n:: set {RandomVar.ToLower()}={RandValue.ToLower()}");
+                }
+            }
+            File.AppendAllText($"Obfuscated_{Name}", "%?%@%?%e%?%c%?%h%?%o o%?%f%?%f");
+
+            Content = Content.Replace("@echo off", "");
+            var builder = new StringBuilder();
+            int count = 0;
+            foreach(char letter in Salt)
+            {
+                foreach (var c in Content)
+                {
+                    builder.Append(c);
+                    if ((++count % 1) == 0)
+                    {
+                        builder.Append("%" + letter.ToString() + "%");
+                    }
+                }
+            }
+            Content = builder.ToString();
+            File.AppendAllText($"Obfuscated_{Name}", Content);
+            File.AppendAllText($"Obfuscated_{Name}", "\n\n");
+
+            for (int x = 0; x <= 100; x++)
+            {
+                string ContentOB = File.ReadAllText($"Obfuscated_{Name}");
+                string RandValue = RandomStringer();
+                string RandomVar = RandomVariabler();
+
+                if (!ContentOB.Contains(RandValue))
+                {
+                    File.AppendAllText($"Obfuscated_{Name}", $"\n:: set {RandomVar.ToLower()}={RandValue.ToLower()}");
+                }
+            }
+            Content = Junk;
+            var buildered = new StringBuilder();
+            int counte = 0;
+            foreach (var c in Content)
+            {
+                builder.Append(c);
+                if ((++counte % 1) == 0)
+                {
+                    builder.Append("→后→");
+                }
+            }
+
+            for (int y = 0; y <= 25; y++)
+            {
+                File.AppendAllText($"Obfuscated_{Name}", ":: " + Junk + "\n:: " + Junk);
+            }
+
+            for (int xe = 0; xe <= 50; xe++)
+            {
+                File.AppendAllText($"Obfuscated_{Name}", ":: " + UTF16arrow + "\n:: " + UTF16arrow);
+            }
+            Console.Beep();
+            MessageBox.Show($"{Name} Has Been Successfully Obfuscated!", "iObfuscator");
+        }
+
+        public static string RandomStringer()
+        {
+            Random r = new Random();
+            string Final = "";
+            string Alphabets = "abcdefghijklmnopqrstuvwxyz";
+            char[] AlphArray = Alphabets.ToCharArray();
+
+            for (int x = 0; x <= 200; x++)
+            {
+                Final = Final + AlphArray[r.Next(0, AlphArray.Length)];
+            }
+
+            return Final;
+        }
+
+        public static string RandomVariabler()
+        {
+            int Length = 1;
+            string Final = "";
+            Random r = new Random();
+            int[] Selecter = { 1, 2, 3, 4, 5 };
+
+            string e = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            char[] Chars = e.ToCharArray();
+            int Decider = Selecter[r.Next(0, Selecter.Length)];
+
+            if (Decider == 0)
+            {
+                Length = 9;
+            }
+            if (Decider == 1)
+            {
+                Length = 8;
+            }
+            if (Decider == 2)
+            {
+                Length = 12;
+            }
+            if (Decider == 3)
+            {
+                Length = 13;
+            }
+            if (Decider == 4)
+            {
+                Length = 25;
+            }
+            if (Decider == 5)
+            {
+                Length = 15;
+            }
+
+            for (int x = 0; x <= Length; x++)
+            {
+                Final = Final + Chars[r.Next(0, Chars.Length)];
+            }
+
+            return Final;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.ShowIcon = false;
+            Console.Beep();
+            MessageBox.Show("The Program Might Freeze While Obfuscating, Which Can Take Some Time...", "iObfuscate");
+        }
+    }
+}
